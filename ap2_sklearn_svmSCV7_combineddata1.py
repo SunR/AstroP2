ba77b3d201641715e6547bd4477ec58cb9e0a41e
SunR <sunnyr2010@gmail.com>
2015-08-21 00:49:42 -0700

    Still trying:

--- ap2_sklearn_svmSCV7_combineddata1.py
+++ ap2_sklearn_svmSCV7_combineddata1.py
@@ -9,18 +9,21 @@ import time
 
 #Trying u-g, g-r, r-i, i-z as only photometric input instead of just raw u, g, r, i, z, still including all 5 spectral lines
 
-#SHOULD I INCLUDE REDSHIFT??
+#SHOULD I INCLUDE REDSHIFT??...Shouldn't really distinguish between elliptical and spiral...
 
 #get just spirals and ellipticals in 1 array, shuffle them, then extract the label column
 data = np.loadtxt("crossmatched3_combineddata1_srane.txt", delimiter = ",", skiprows = 1, usecols = (1, 10, 11, 12, 13, 14, 35, 37, 40, 43, 46, 49, 50, 51)) #dr7objid, petromag_u, petromag_g, petromag_r, pertomag_i, petromag_z, z(redshift),h alpha ew, h beta ew, OII ew, h delta ew, spiral, elliptical, uncertain
 
-data[:,1] = data[:,1] - data[:,2]#subtract u col from r col
+data[:,1] = data[:,1] - data[:,2]#subtract u-g
+data[:,2] = data[:,2] - data[:,3]#subtract g-r
+data[:,3] = data[:,3] - data[:,4]#subtract r-i
+data[:,4] = data[:,4] - data[:,5]#subtract i-z
 
-np.delete(data, 2, 1) #delete 3rd col (0/1 for row/col, 2 for col#)
+np.delete(data, 5, 1) #delete 5th col (5 for col#, 0/1 for row/col)
 
-isSpiral = data[:,8]
-isElliptical = data[:,9] #corresponds to col 24 of real data file, which is the elliptical bool value
-isUncertain = data[:,10]
+isSpiral = data[:,10]#with one col removed!!
+isElliptical = data[:11] #corresponds to the elliptical bool value
+isUncertain = data[:,12]
 
 ellipticals = data[isElliptical == 1]
 
@@ -32,9 +35,9 @@ trainingSetSpirals = spirals[:50000] #extracting first 5000 spiral and elliptica
 
 trainingSet = np.vstack((trainingSetEllipticals, trainingSetSpirals))  #using only elliptical and spiral for training
 np.random.shuffle(trainingSet)
-trainingSetLabels = trainingSet[:,9]  #putting labels in separate array
+trainingSetLabels = trainingSet[:,11]  #putting labels in separate array
 trainingSetLabels[trainingSetLabels == 0] = -1 #replacing all 0 with -1 to match sklearn format
-trainingSet = trainingSet[:, 1:8] #removing label cols from actual inputs
+trainingSet = trainingSet[:, 1:10] #removing label cols from actual inputs
 startTime = time.time()
 print "Time before training = ", startTime
 
@@ -59,20 +62,20 @@ testingSetSpirals = spirals[50000:100000]
 
 testingSet = np.vstack((testingSetEllipticals, testingSetSpirals))  #using only elliptical and spiral for training
 np.random.shuffle(testingSet)
-testingSetLabels = testingSet[:,9]  #putting labels in separate array
+testingSetLabels = testingSet[:,11]  #putting labels in separate array
 testingSetLabels[testingSetLabels == 0] = -1 #replacing all 0 with -1 to match sklearn format
-testingSet = testingSet[:, 1:8] #removing label cols from actual inputs
+testingSet = testingSet[:, 1:10] #removing label cols from actual inputs
 
 testingAccuracy = clf.score(testingSet, testingSetLabels)
 print "Testing accuracy = ", testingAccuracy
 print "Time = ", time.time() - startTime, "seconds"
 
-classificationSet = uncertains[:, 1:8]
+classificationSet = uncertains[:, 1:10]
 predictions = clf.predict(classificationSet) #use model to classify uncertains - SEE IF YOU CAN FIND CONFIDENCE INTERVAL FOR THIS PREDICTION AFTERWARD, THAT WILL BE PROGRESS
 
-uncertainsPredictions = np.column_stack((uncertains[:, 0:8], predictions)) #hstack doesn't work here, b/c multidimensional array?
+uncertainsPredictions = np.column_stack((uncertains[:, 0:10], predictions)) #hstack doesn't work here, b/c multidimensional array?
 
-f = open('uncertainsPredictionsOutput6.txt', 'w') #output 4 - run 1, output5 - run2 of this program
+f = open('uncertainsPredictionsOutputColors.txt', 'w') #output 4 - run 1, output5 - run2 of this program
 
 uncertainsPredictions.tofile(f, sep=",", format="%f") #export array to file in floating point values, comma separated
 print "Finished writing predictions to file!"
