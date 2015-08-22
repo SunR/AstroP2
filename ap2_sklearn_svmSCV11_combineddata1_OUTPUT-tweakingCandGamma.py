93543872ae463db37d5f2caaccc9966d56acf3eb
SunR <sunnyr2010@gmail.com>
2015-08-22 00:36:45 -0700

    -C

---
+++ ap2_sklearn_svmSCV11_combineddata1_OUTPUT-tweakingCandGamma.py
@@ -0,0 +1,131 @@
+Python 2.7.10 (default, May 23 2015, 09:44:00) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440229314.63
+SVC(C=10, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+  gamma=0.0001, kernel='rbf', max_iter=-1, probability=False,
+  random_state=None, shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  271.561999798 seconds
+Training accuracy =  0.88261
+Time =  369.194999933 seconds
+
+Testing accuracy =  0.853231779298
+Time =  430.040999889 seconds
+Finished writing predictions to file!
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440230206.28
+SVC(C=10, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0,
+  kernel='rbf', max_iter=-1, probability=False, random_state=None,
+  shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  968.4599998 seconds
+Training accuracy =  0.93223
+Time =  1053.96999979 seconds
+
+Testing accuracy =  0.896246705154
+Time =  1102.03399992 seconds
+
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440231606.32
+SVC(C=100, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0,
+  kernel='rbf', max_iter=-1, probability=False, random_state=None,
+  shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  2145.421 seconds
+Training accuracy =  0.94864
+Time =  2217.91799998 seconds
+
+Testing accuracy =  0.899691133427
+Time =  2266.6730001 seconds
+Finished writing predictions to file!
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440234909.49
+SVC(C=1000, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0,
+  kernel='rbf', max_iter=-1, probability=False, random_state=None,
+  shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  9387.10099983 seconds
+Training accuracy =  0.96009
+Time =  9454.02599978 seconds
+
+Testing accuracy =  0.888225876874
+Time =  9495.08299994 seconds
+Finished writing predictions to file!
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440258025.4
+SVC(C=100, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+  gamma=0.0001, kernel='rbf', max_iter=-1, probability=False,
+  random_state=None, shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  208.207000017 seconds
+Training accuracy =  0.9014
+Time =  278.453000069 seconds
+
+Testing accuracy =  0.888775691716
+Time =  320.644000053 seconds
+
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440258475.3
+SVC(C=100, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+  gamma=1e-07, kernel='rbf', max_iter=-1, probability=False,
+  random_state=None, shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  470.138999939 seconds
+Training accuracy =  0.8314
+Time =  600.23300004 seconds
+
+Testing accuracy =  0.762819579877
+Time =  687.994999886 seconds
+Finished writing predictions to file!
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440260443.36
+SVC(C=100, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.1,
+  kernel='rbf', max_iter=-1, probability=False, random_state=None,
+  shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  1815.53399992 seconds
+Training accuracy =  0.94863
+Time =  1885.75900006 seconds
+
+Testing accuracy =  0.899642620353
+Time =  1929.92600012 seconds
+
+>>> ================================ RESTART ================================
+>>> 
+started knd
+finished knd
+50000
+Time before training =  1440262408.86
+SVC(C=100, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=1.0,
+  kernel='rbf', max_iter=-1, probability=False, random_state=None,
+  shrinking=True, tol=0.001, verbose=False)
+Done training! Time =  5669.28700018 seconds
+Training accuracy =  0.99524
+Time =  5921.80700016 seconds
+
+Testing accuracy =  0.869257264833
+Time =  6075.30800009 seconds
+Finished writing predictions to file!
+>>> 
