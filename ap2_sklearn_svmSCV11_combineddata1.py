2b7b63b70e87a267043b096cd024eb5b87985323
SunR <sunnyr2010@gmail.com>
2015-08-21 17:50:45 -0700

    Using density on its own

--- ap2_sklearn_svmSCV11_combineddata1.py
+++ ap2_sklearn_svmSCV11_combineddata1.py
@@ -19,19 +19,19 @@ coords = data[:, 1:3]
 
 print coords.shape
 
-knd = KNeighborsDensity("bayesian", 10)
+knd = KNeighborsDensity("bayesian", 5) #try using something other than 10 for n_neighbors values, to experiment + optimize
 knd.fit(coords)
 density = knd.eval(coords)
 
 data[:, 1] = density
 
-data = np.delete(data, 2, 1) #(col# 2 , 0/1 for row/col)
+sdata = np.delete(data, 2, 1) #(col# 2 , 0/1 for row/col)
 
-isSpiral = data[:,2]#with one col removed!!
-print data[:5, :]
-isElliptical = data[:,3] #corresponds to the elliptical bool value
+isSpiral = data[:,7]#with one col removed!!
+#print data[:5, :]
+isElliptical = data[:,8] #corresponds to the elliptical bool value
 
-isUncertain = data[:,4]
+isUncertain = data[:,9]
 
 ellipticals = data[isElliptical == 1]
 
@@ -43,7 +43,7 @@ trainingSetSpirals = spirals[:50000] #extracting first 5000 spiral and elliptica
 
 trainingSet = np.vstack((trainingSetEllipticals, trainingSetSpirals))  #using only elliptical and spiral for training
 np.random.shuffle(trainingSet)
-trainingSetLabels = trainingSet[:,3]  #putting labels in separate array
+trainingSetLabels = trainingSet[:,8]  #putting labels in separate array
 
 trainingSetLabels[trainingSetLabels == 0] = -1 #replacing all 0 with -1 to match sklearn format
 counter = 0
@@ -51,7 +51,7 @@ for i in trainingSetLabels:
     if i == -1:
         counter +=1
 print counter
-trainingSet = trainingSet[:, 1:2] #removing label cols from actual inputs
+trainingSet = trainingSet[:, 1:7] #removing label cols from actual inputs
 startTime = time.time()
 print "Time before training = ", startTime
 
@@ -76,18 +76,18 @@ testingSetSpirals = spirals[50000:100000]
 
 testingSet = np.vstack((testingSetEllipticals, testingSetSpirals))  #using only elliptical and spiral for training
 np.random.shuffle(testingSet)
-testingSetLabels = testingSet[:,3]  #putting labels in separate array
+testingSetLabels = testingSet[:,8]  #putting labels in separate array
 testingSetLabels[testingSetLabels == 0] = -1 #replacing all 0 with -1 to match sklearn format
-testingSet = testingSet[:, 1:2] #removing label cols from actual inputs
+testingSet = testingSet[:, 1:7] #removing label cols from actual inputs
 
 testingAccuracy = clf.score(testingSet, testingSetLabels)
 print "Testing accuracy = ", testingAccuracy
 print "Time = ", time.time() - startTime, "seconds"
 
-classificationSet = uncertains[:, 1:2]
+classificationSet = uncertains[:, 1:7]
 predictions = clf.predict(classificationSet) #use model to classify uncertains - SEE IF YOU CAN FIND CONFIDENCE INTERVAL FOR THIS PREDICTION AFTERWARD, THAT WILL BE PROGRESS
 
-uncertainsPredictions = np.column_stack((uncertains[:, 0:2], predictions)) #hstack doesn't work here, b/c multidimensional array?
+uncertainsPredictions = np.column_stack((uncertains[:, 0:7], predictions)) #hstack doesn't work here, b/c multidimensional array?
 
 f = open('uncertainsPredictionsOutputDensity1.txt', 'w') 
 
