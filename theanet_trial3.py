61e56768ccf9ee3298f15df3a0ae6b13f245fa75
SunR <sunnyr2010@gmail.com>
2015-08-28 03:09:45 -0700

    vel disp

--- theanet_trial3.py
+++ theanet_trial3.py
@@ -1,21 +1,3 @@
-'''
-Theanets "Hello, world!" - train a simple neural network for classifying
-simple data and evaluate the results.
-[Theanets](https://github.com/lmjohns3/theanets) allows to build and train
-neural networks on top of the [Theano](https://github.com/Theano/Theano)
-compiler.
-The goal is to get familiar with theanets on some simple example. You can
-modify this example bit by bit to work on more complex data and models.
-In this example we generate some synthetic data (via scikit-learn) - two 2D
-blobs with Gaussian distribution which are in addition linearly separable.
-Thus any classification model should have no problem with such data.
-We create a neural network with three layers - input, hidden and output - each
-with two dimensions (2D featues, two classes). The input and hidden layer has
-by default sigmoid activation, the output clasification layer has softmax
-actiovation by default. The model is trained via the stochastic gradient descent
-algorithm.
-Finally the model is evaluated by functions provided by scikit-learn.
-'''
 
 #Trying old dummy model with SDSS inputs
 
@@ -27,10 +9,11 @@ import numpy as np
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
+import time
 
-climate.enable_default_logging()
+#climate.enable_default_logging()
 
-# -- generate some data --
+startTime = time.time()
 
 data = np.loadtxt("crossmatched3_combineddata1_srane.txt", delimiter = ",", skiprows = 1, usecols = (1, 10, 11, 12, 13, 14, 35, 37, 39, 41, 43, 49, 50, 51)) #dr7objid, petromag_u, petromag_g, petromag_r, pertomag_i, petromag_z, z(redshift),h alpha ew, h beta ew, OII ew, h delta ew, spiral, elliptical, uncertain
 
@@ -58,25 +41,12 @@ trainingSet = trainingSet[:, 1:11] #removing label cols from actual inputs
 X = trainingSet
 y = trainingSetLabels
 
-# very simple data - two linearly separable 2D blobs
-n_samples = 1000
 # centers - number of classes
 # n_features - dimension of the data
 
 # convert the features and targets to the 32-bit format suitable for the model
 X = X.astype(np.float32)
 y = y.astype(np.int32)
-
-# -- visualize the data for better understanding --
-
-def plot_2d_blobs(dataset):
-    X, y = dataset
-    plt.axis('equal')
-    plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.1, edgecolors='none')
-    plt.show()
-
-# plot the blobs in interactive mode
-#plot_2d_blobs((X, y))
 
 # -- split the data into training, validation and test sets --
 
@@ -93,15 +63,13 @@ def split_data(X, y, slices):
         'validation': slice(starts[1], starts[2]),
         'test': slice(starts[2], None)}
     data = X, y
-    def slice_data(data, sl):
-        return tuple(d[sl] for d in data)
+    def slice_data(data, sli):
+        return tuple(d[sli] for d in data)
     for label in slices:
         datasets[label] = slice_data(data, slices[label])
     return datasets
 
 datasets = split_data(X, y, (0.6, 0.2, None))
-
-# -- create and train the model --
 
 # plain neural network with a single hidden layer
 exp = theanets.Experiment(
@@ -118,6 +86,7 @@ exp.train(
     momentum=0.5,
     hidden_l1=0.1)
 
+print "Done training! Time = ", time.time()-startTime, "seconds"
 # -- evaluate the model on test data --
 
 X_test, y_test = datasets['test']
