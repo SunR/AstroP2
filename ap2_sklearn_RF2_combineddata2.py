51aba516cda350c7fb795a954545f7c6f22cee54
SunR <sunnyr2010@gmail.com>
2015-08-29 06:02:40 -0700

    RF at 96% with new datagit add -A! :)

--- ap2_sklearn_RF2_combineddata2.py
+++ ap2_sklearn_RF2_combineddata2.py
@@ -15,17 +15,20 @@ import time
 #Photometric and spectral data, random forest classifer
 
 print "Random Forest Classifier"
-data = np.loadtxt("crossmatched3_combineddata1_srane.txt", delimiter = ",", skiprows = 1, usecols = (37,)) #after row 35 creates the problems..
-
-print data[:5]
 
 #get just spirals and ellipticals in 1 array, shuffle them, then extract the label column
-data = np.loadtxt("combined2_w_velDisp_srane2.txt", delimiter = ",", skiprows = 1, usecols = (1, 10, 11, 12, 13, 14, 35, 37, 39, 41, 43, 49, 50, 51)) #58, 
+data = np.loadtxt("combined1_half1_test3.txt", delimiter = ",", skiprows = 1, usecols = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36)) #58, 
 
-isSpiral = data[:,12]
-isElliptical = data[:,13] #corresponds to the elliptical bool value
+data2 = np.loadtxt("combined1_half2_test3.txt", delimiter = ",", skiprows = 1, usecols = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36)) #58, 
 
-isUncertain = data[:,14]
+data = np.vstack((data, data2)) #combined two split csv files of data
+
+print "stacked files!"
+
+isSpiral = data[:,30]
+isElliptical = data[:,31] #corresponds to the elliptical bool value
+
+isUncertain = data[:,32]
 
 ellipticals = data[isElliptical == 1]
 
@@ -37,11 +40,11 @@ trainingSetSpirals = spirals
 
 trainingSet = np.vstack((trainingSetEllipticals, trainingSetSpirals))  #using only elliptical and spiral for training
 np.random.shuffle(trainingSet)
-trainingSetLabels = trainingSet[:,13]  #putting labels in separate array
+trainingSetLabels = trainingSet[:,31]  #putting labels in separate array
 
 trainingSetLabels[trainingSetLabels == 0] = -1 #replacing all 0 with -1 to match sklearn format
 
-trainingSet = trainingSet[:, 1:12] #removing label cols from actual inputs
+trainingSet = trainingSet[:, :30] #removing label cols from actual inputs
 
 trainingSet, testingSet, trainingSetLabels, testingSetLabels = train_test_split(trainingSet, trainingSetLabels, test_size = 0.6, random_state = 0) #fixes random_state so results reproducible
 
